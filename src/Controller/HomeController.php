76e9aaf6d4f0a980abd2a78d80ff104069237071
angelique KRAUSE <krause_angelique@hotmail.com>
2023-12-25 20:00:16 +0100

    add templates home index.html.twig and base.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    // ma route pour la page home
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        // mon controlleur pour la page home
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+    #[Route('/test', name: 'app_test')]
+    public function test(): Response
+    {
+        return $this->render('home/test.html.twig', []);
     }
 }
